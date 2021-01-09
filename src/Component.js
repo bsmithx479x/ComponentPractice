9c1a9de186ee2e80c3c98c6f4ab4f4bc503fa381
unknown <bsmithx479x@gmail.com>
2021-01-09 16:37:31 -0600

    committing

---
+++ src/Component.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Component.css';
+
+class MyComponent extends React.Component {
+    constructor(props) {
+    super(props);
+    }
+
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>Nikki Gay</li>
+                    <li>Jee Gay</li>
+                    <li>Marty Gay</li>
+                    <li>I'm Gay</li>
+                </ul>
+            </nav>
+        );
+    }
+};
+
+
+
+
+export default MyComponent;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Component from './Component';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Component />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
